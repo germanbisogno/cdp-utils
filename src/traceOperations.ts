9f53c5d693a22d849a757fb1c8d1cf9f6d2e0114
German Bisogno <german.bisogno@globant.com>
2022-01-07 10:22:53 -0300

    fix abstract class

--- src/traceOperations.ts
+++ src/traceOperations.ts
@@ -2,10 +2,9 @@ import Protocol from "devtools-protocol";
 import { Har } from "har-format";
 
 export abstract class TraceOperations {
+    abstract startTrace(): Promise<void> | Promise<Protocol.Performance.GetMetricsResponse>;
     abstract stopTrace(): Promise<Protocol.Runtime.ConsoleAPICalledEvent[]>
         | Promise<Protocol.Performance.GetMetricsResponse>
         | Promise<Protocol.Tracing.DataCollectedEvent[]>
         | Promise<Har>;
-
-    abstract startTrace(): Promise<void> | Promise<Protocol.Performance.GetMetricsResponse>;
 }
