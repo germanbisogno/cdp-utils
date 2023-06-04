3b352fda5213aae52792faef30162e7cf2009238
GERMAN <germanbisogno@gmail.com>
2023-06-04 18:12:51 -0300

    log response data

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,25 @@
 import * as winston from 'winston';
 
+const logFileLevel = 'debug';
+
+const alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.label({
+    label: '[LOGGER]',
+  }),
+  winston.format.timestamp({
+    format: 'YY-MM-DD HH:mm:ss',
+  }),
+  winston.format.printf(
+    (info) =>
+      ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`
+  )
+);
+
 export const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
+  level: logFileLevel,
+  format: winston.format.combine(winston.format.colorize(), alignColorsAndTime),
 });
